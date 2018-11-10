f32d4743c643b268301b371596e09af2a27d97ab
sudheerachary <sudheerachary115@gmail.com>
2018-11-10 19:07:52 +0530

    add confusion matrix, precision, recall etc on predictions

--- src/NaiveBayes.py
+++ src/NaiveBayes.py
@@ -1,4 +1,6 @@
+import itertools
 import numpy as np
+import matplotlib.pyplot as plt
 np.random.seed(1234)
 
 from FeatureGenerator import FeatureGenerator
@@ -6,6 +8,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import ComplementNB
 from sklearn.naive_bayes import BernoulliNB
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
 
 
 class NaiveBayes:
@@ -112,87 +118,83 @@ class NaiveBayes:
 
 
     def train(self):
-        """
-        issues:
-            - more misclassifications
-            - Bernoulli Accuracy: 68.4
-            - Multinomial Accuracy: 59.1
-            - Complement Accuracy: 49.3
-            - Gaussian Accuracy: 22.0
-
-        """
-        print("Training on %d papers" %(len(self.train_papers)))
+        """ """
+        print("=== Training on %d papers ===" %(len(self.train_papers)))
         if self.distribution == "Multinomial": 
             self.mnb = MultinomialNB()
             y_pred = self.mnb.fit(self.train_X, self.train_y).predict(self.train_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.train_X.shape[0],(self.train_y != y_pred).sum()))
-            print("Train Accuracy: %f"
-                % (self.accuracy((self.train_y != y_pred).sum(), self.train_X.shape[0])))
-
         elif self.distribution == "Gaussian":
             self.gnb = GaussianNB()
             y_pred = self.gnb.fit(self.train_X, self.train_y).predict(self.train_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.train_X.shape[0],(self.train_y != y_pred).sum()))
-            print("Train Accuracy: %f"
-                % (self.accuracy((self.train_y != y_pred).sum(), self.train_X.shape[0])))
-
         elif self.distribution == "Complement":
             self.cnb = ComplementNB()
             y_pred = self.cnb.fit(self.train_X, self.train_y).predict(self.train_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.train_X.shape[0],(self.train_y != y_pred).sum()))
-            print("Train Accuracy: %f"
-                % (self.accuracy((self.train_y != y_pred).sum(), self.train_X.shape[0])))
-
         elif self.distribution == "Bernoulli":
             self.bnb = BernoulliNB()
             y_pred = self.bnb.fit(self.train_X, self.train_y).predict(self.train_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.train_X.shape[0],(self.train_y != y_pred).sum()))
-            print("Train Accuracy: %f"
-                % (self.accuracy((self.train_y != y_pred).sum(), self.train_X.shape[0])))                   
-
 
     def test(self):
         """ """
-        print("Testing on %d papers" %(len(self.test_papers)))
+        print("=== Testing on %d papers ===" %(len(self.test_papers)))
         if self.distribution == "Multinomial": 
             self.mnb = MultinomialNB()
             y_pred = self.mnb.fit(self.test_X, self.test_y).predict(self.test_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.test_X.shape[0],(self.test_y != y_pred).sum()))
-            print("Test Accuracy: %f"
-                % (self.accuracy((self.test_y != y_pred).sum(), self.test_X.shape[0])))
-
         elif self.distribution == "Gaussian":
             self.gnb = GaussianNB()
             y_pred = self.gnb.fit(self.test_X, self.test_y).predict(self.test_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.test_X.shape[0],(self.test_y != y_pred).sum()))
-            print("Test Accuracy: %f"
-                % (self.accuracy((self.test_y != y_pred).sum(), self.test_X.shape[0])))
-
         elif self.distribution == "Complement":
             self.cnb = ComplementNB()
             y_pred = self.cnb.fit(self.test_X, self.test_y).predict(self.test_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.test_X.shape[0],(self.test_y != y_pred).sum()))
-            print("Test Accuracy: %f"
-                % (self.accuracy((self.test_y != y_pred).sum(), self.test_X.shape[0])))
-
         elif self.distribution == "Bernoulli":
             self.bnb = BernoulliNB()
             y_pred = self.bnb.fit(self.test_X, self.test_y).predict(self.test_X)
-            print("Number of mislabeled sentences out of a total %d sentences : %d"
-                  % (self.test_X.shape[0],(self.test_y != y_pred).sum()))
-            print("Test Accuracy: %f"
-                % (self.accuracy((self.test_y != y_pred).sum(), self.test_X.shape[0])))
+        return (self.getConfusionMatrix(self.test_y, y_pred),
+            self.getPrecisionRecallF1Score(self.test_y, y_pred),
+            self.getAccuracy(self.test_y, y_pred))
+    
+
+    def plotConfusionMatrix(self, cm, classes, normalize=True, title='Confusion matrix', cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.show()
+
+    def getConfusionMatrix(self, y_true, y_pred):
+        return confusion_matrix(y_true, y_pred)
 
 
-    def accuracy(self, misclassifications, samples):
-        return (1-(misclassifications/(samples*1.0)))*100.0
+    def getPrecisionRecallF1Score(self, y_true, y_pred):
+        return precision_recall_fscore_support(y_true, y_pred, average='micro')
+
+
+    def getAccuracy(self, y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
 
 
 if __name__ == '__main__':
@@ -200,4 +202,6 @@ if __name__ == '__main__':
     featureGen = FeatureGenerator(xmlcorpora)
     classifer = NaiveBayes(features=featureGen.features, distribution="Bernoulli", train_split=0.85)
     classifer.train()
-    classifer.test()
+    confusionMatrix, precisionRecallF1, accuracy = classifer.test()
+    classifer.plotConfusionMatrix(confusionMatrix, range(8))
+    print("=== Accuracy: %f ===" %(accuracy))
